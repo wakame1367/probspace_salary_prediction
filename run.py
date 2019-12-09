f2ed025aa569fcc5e247f610fb2688431a8d34c8
kot.yuhara <kot.yuhara@trifort.jp>
2019-12-09 11:49:38 +0900

    baseline

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+import category_encoders as ce
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import KFold
+from sklearn.preprocessing import LabelEncoder
 
 dataset_path = Path("resources")
 train_path = dataset_path / "train_data.csv"
@@ -17,7 +19,8 @@ def submit(predictions):
         "submission.csv", index=False)
 
 
-def lgb_cv(_train, _test, _target, lgb_params, cat_idx, fold_schema):
+def lgb_cv(_train, _test, _target, model_params, train_params, cat_idx,
+           fold_schema):
     oof = np.zeros(len(_train))
     predictions = np.zeros(len(_test))
 
@@ -27,18 +30,18 @@ def lgb_cv(_train, _test, _target, lgb_params, cat_idx, fold_schema):
                                label=_target.iloc[trn_idx])
         val_data = lgb.Dataset(_train.iloc[val_idx],
                                label=_target.iloc[val_idx])
-        model = lgb.train(lgb_params,
+        model = lgb.train(model_params,
                           trn_data,
                           valid_names=['train', 'valid'],
                           valid_sets=[trn_data, val_data],
                           categorical_feature=cat_idx,
-                          verbose_eval=200)
+                          **train_params)
         oof[val_idx] = model.predict(_train.iloc[val_idx],
                                      num_iteration=model.best_iteration)
         print(mean_absolute_error(np.expm1(_target.iloc[val_idx]),
                                   np.expm1(oof[val_idx])))
         predictions += model.predict(_test,
-                                     num_iteration=model.best_iteration) / kf.n_splits
+                                     num_iteration=model.best_iteration) / fold_schema.n_splits
     print(mean_absolute_error(np.expm1(_target), np.expm1(oof)))
 
     return predictions
@@ -48,12 +51,24 @@ def main():
     train = pd.read_csv(train_path)
     test = pd.read_csv(test_path)
 
-    #
-    train['city'] = train['area'].apply(
-        lambda x: 1 if '東京' in x or '大阪' in x else 0)
+    target_col = "salary"
+    target = train[target_col]
+    target = target.map(np.log1p)
+    train.drop(columns=[target_col], inplace=True)
 
+    train["is_test"] = 0
+    test["is_test"] = 1
+    data = pd.concat([train, test])
     #
-    train[train['age'] >= 60]['age'] = 60
+    # data['city'] = data['area'].apply(
+    #     lambda x: 1 if '東京' in x or '大阪' in x else 0)
+    area_encoder = LabelEncoder()
+    area_encoder.fit(data["area"])
+    data["area"] = area_encoder.transform(data["area"])
+    #
+    data[data['age'] >= 60]['age'] = 60
+
+    # id 8117
 
     n_splits = 4
     kf = KFold(n_splits=n_splits, random_state=71, shuffle=True)
@@ -62,6 +77,7 @@ def main():
         'boosting_type': 'gbdt',
         'objective': 'fair',
         'metric': 'fair',
+        'n_estimators': 20000,
         'num_leaves': 31,
         'learning_rate': 0.01,
         'feature_fraction': 0.9,
@@ -73,21 +89,33 @@ def main():
 
     train_params = {
         'early_stopping_rounds': 200,
-        'n_estimators': 20000,
         'verbose_eval': -1
     }
 
     # id is wasted col
     drop_cols = ["id", "service_length"]
 
-    train.drop(columns=drop_cols, inplace=True)
-    test.drop(columns=drop_cols, inplace=True)
+    data.drop(columns=drop_cols, inplace=True)
 
-    target_col = "salary"
     cat_features_cols = ["position", "area", "sex", "partner", "education"]
+    cat_features_idx = [idx for idx, col in enumerate(train.columns) if
+                        col in cat_features_cols]
+    cat_boost_enc = ce.CatBoostEncoder(cols=cat_features_cols)
+    ord_enc = ce.OrdinalEncoder(cols=cat_features_cols)
+    count_enc = ce.CountEncoder(cols=cat_features_cols)
+    # data[cat_features_cols] = ord_enc.fit_transform(data[cat_features_cols])
+    # data[cat_features_cols] = count_enc.fit_transform(data[cat_features_cols])
 
-    target = train[target_col]
-    train.drop(columns=[target_col], inplace=True)
+    train = data[data["is_test"] == 0]
+    test = data[data["is_test"] == 1]
+    train.drop(columns=["is_test"], inplace=True)
+    test.drop(columns=["is_test"], inplace=True)
+
+    predictions = lgb_cv(train, test, target,
+                         model_params=model_params,
+                         train_params=train_params,
+                         cat_idx=cat_features_idx,
+                         fold_schema=kf)
 
 
 if __name__ == '__main__':
